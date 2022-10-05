971cbd1cfbadedb451769e9b05bf0b6645c83e0d
TuralAbdullaev <tural.abdullaev@mail.ru>
2022-10-05 11:39:52 +0300

    array task

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+Console.WriteLine("Введите массив строк через пробел");
+string stroka = Console.ReadLine();
+string[] Array3(string stroka)
+{
+    string[] M = stroka.Split(' ');
+    string[] result = new string[M.Length];
+    int realSize = 0;
+    foreach (var value in M)
+    {
+        if (value.Length <= 3)
+        {
+            result[realSize] = value;
+            realSize++;
+        }
+
+    }
+    return result;
+}
+
+string[] array = Array3(stroka);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+PrintArray(array);
